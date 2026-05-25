396e83f94007bcfc7d598434d87cc7e96731e54e
monju1125 <monju1125@github.com>
2026-05-25 02:29:20 +0000

    Test run: 0/1 passed, 1 failed, 0 skipped - 2026-05-25T02:29:19.986Z

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -21,7 +21,8 @@ public class Runner {
      *
      * @param in the String value you should change global_var to in.
      */
-    public void changeClassVar(String global_var){
+    static String in ;
+    public void changeClassVar(String in){
         
     }
 }
